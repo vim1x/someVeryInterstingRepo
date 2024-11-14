cb9fe52ad4023fd1f50e2be6d666f9490d8abdaa
vim1x <v1mix@protonmail.com>
2024-11-14 15:28:55 +0200

    Add screenshot

--- src/test/java/Listeners/AllureListener.java
+++ src/test/java/Listeners/AllureListener.java
@@ -2,19 +2,30 @@ package Listeners;
 
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Allure;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import java.io.File;
+import java.io.IOException;
 
 
 public class AllureListener implements ITestListener {
     @Override
     public void onTestFailure(ITestResult iTestResult) {
-        Allure.attachment("Screenshot.png", Selenide.screenshot(OutputType.BASE64));
+        createFullPageScreenshot();
     }
 
+    private void createFullPageScreenshot() {
+        File file = Selenide.screenshot(OutputType.FILE);
+        try {
+            Allure.addAttachment("Screenshot", FileUtils.openInputStream(file));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 
 }
